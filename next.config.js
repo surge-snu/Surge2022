fd2a37f8197fded1ebcc68b7269240fa335d0930
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-22 18:11:24 +0530

    feat: Automatic mixin import

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack(config) {
+    config.module.rules[2].oneOf?.forEach((one) => {
+      if (!`${one.issuer?.and}`.includes("_app")) return;
+      one.issuer.and = [path.resolve(__dirname)];
+    });
+    return config;
+  },
+  sassOptions: {
+    additionalData: `@import "styles/root/_mixins.scss";`,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
