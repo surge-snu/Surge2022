9c3f6ffe7e560e266330c5bf4ba46413658b6ac0
keshavdubeysnu <89126219+keshavdubeysnu@users.noreply.github.com>
2022-07-25 13:58:10 +0530

    scrolling banner & about section complete

---
+++ pages/sportScroll.js
@@ -0,0 +1,64 @@
+import "../styles/root/sportScroll.scss";
+
+export default function Banner() {
+  return (
+    <div class="slider">
+      <div class="slide-track">
+        <div class="slide">
+          <h1 className="sport_text_1">Football</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Cricket</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Badminton</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Squash</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Basketball</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Vollyball</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Football</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Cricket</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Badminton</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Squash</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Basketball</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Vollyball</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Football</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Cricket</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Badminton</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Squash</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_1">Basketball</h1>
+        </div>
+        <div class="slide">
+          <h1 className="sport_text_2">Vollyball</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
