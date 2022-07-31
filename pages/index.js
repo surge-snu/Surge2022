3abe40da173fdb1c9f8feedcbeb42e47e582528c
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-31 15:03:18 +0530

    chore: imported fonts

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,14 @@ export default function Home() {
     console.log(data);
   };
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Surge 2022</title>
         <meta name="description" content="Awesome website for Surge" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>Surge 2022!</main>
-      <button onClick={handler}>Ping</button>
-    </div>
+      <main className="LandingPage"></main>
+    </>
   );
 }
