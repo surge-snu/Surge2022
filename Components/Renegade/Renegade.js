6d038abe17616f3f3d1d9a586d31db99372a9984
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-01 00:48:24 +0530

    feat: Responsive renagade page

--- Components/Renegade/Renegade.js
+++ Components/Renegade/Renegade.js
@@ -1,39 +1,31 @@
 import React from "react";
 import Image from "next/dist/client/image";
 import RenegadeCard from "../RenegadeCard/RenegadeCard";
+import "./Renegade.scss";
 
 export default function Renegade() {
-	
-	return (
-		<div className="Renegade">
-			<h1 className="Renegade__title">Renegade</h1>
-			<div className="Renegade__subtitleDiv">
-				<h3 className="Renegade__subtitle">WE ARE THE
-					<span className="Renegade__subtitle--green"> KING </span>
-					IN</h3>
-				<h3 className="Renegade__subtitle">EVERY GAME</h3>
-			</div>
-			<div className="Renegade__desc">
-				<p className="Renegade__descText">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-				</p>
-			</div>
-			<div className="Renegade__cards">
-				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => (
-					<RenegadeCard key={i} />
-				))}
-			</div>
-			<div className="Renegade__explore">
-				<p>EXPLORE GALLERY</p>
-				<div className="Renegade__rightArrow">
-					<Image
-						src="/RightArrow.svg"
-						alt="Hero Image"
-						layout="fill"
-						objectFit="contain"
-					/>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="Renegade">
+      <div className="Renegade__top">
+        <h2 className="Renegade__top--outlineTitle">Renegade</h2>
+        <h2 className="Renegade__top--title">
+          WE ARE THE<span> KING</span> IN EVERY GAME
+        </h2>
+      </div>
+      <p className="Renegade__middle">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud
+      </p>
+      <div className="Renegade__cards">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (
+          <RenegadeCard key={i} />
+        ))}
+      </div>
+      <div className="Renegade__bottom">
+        <p>EXPLORE GALLERY</p>
+        <img className="Renegade__bottom--arrow" src="/Img/RightArrow.svg" />
+      </div>
+    </div>
+  );
 }
