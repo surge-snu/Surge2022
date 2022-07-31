a24936aedef917db7e368a84f921cc2677f1f9d7
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-31 15:34:01 +0530

    chore: Changes path

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
+
+import Header from "../Components/Header/Header";
+import SportScroll from "../Components/SportScroll/SportScroll";
 import "../styles/routes/Home.scss";
 
 export default function Home() {
@@ -15,7 +17,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="LandingPage"></main>
+      <main className="LandingPage">
+        <Header />
+        <SportScroll />
+      </main>
     </>
   );
 }
