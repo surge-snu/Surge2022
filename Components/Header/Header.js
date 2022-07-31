83d29b4bd8910f685c59fca70820d92a8cdd53f6
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-31 15:34:20 +0530

    feat(Component): Header init

---
+++ Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Header.scss";
+
+function Header() {
+  return <div className="HeaderWrapper">Test content</div>;
+}
+
+Header.propTypes = {};
+
+Header.defaultProps = {};
+
+export default Header;
