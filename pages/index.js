c4b827a0fdc0c91f9e9d20b75c28dfbf2e3c3783
ABSanthosh <a.b.santhosh02@gmail.com>
2022-07-31 20:32:59 +0530

    feat: Responsive hero section

--- pages/index.js
+++ pages/index.js
@@ -1,70 +1,62 @@
 import Head from "next/head";
 import Image from "next/image";
-import Renegade from "../components/Renegade/Renegade";
+import Renegade from "../Components/Renegade/Renegade";
 
 import Header from "../Components/Header/Header";
 import SportScroll from "../Components/SportScroll/SportScroll";
 import "../styles/routes/Home.scss";
- 
+
 export default function Home() {
   const handler = async () => {
     const data = await fetch("/api/notify-list?email=asd");
     console.log(data);
   };
   return (
-    <div className="LandingPage">
+    <>
       <Head>
         <title>Surge 2022</title>
         <meta name="description" content="Awesome website for Surge" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-			<section className="HeroSection">
-				<div className="HeroSection__background">
-					<Image
-						src="/LandingBackground.svg"
-						alt="Background Image"
-						layout="fill"
-						objectFit="cover"
-					/>
-				</div>
-				<div className="HeroSection__image">
-					<div className="HeroSection__imageOverlay" />
-					<Image
-						src="/crop-legs-kicking-ball-smoke.svg"
-						alt="Hero Image"
-						layout="fill"
-						objectFit="cover"
-						objectPosition={"Right"}
-					/>
-				</div>
-				<div className="HeroSection__scrollButtonDiv">
-					<div className="HeroSection__scrollButtonImage">
-						<Image
-							src="/DownArrow.svg"
-							alt="Hero Image"
-							layout="fill"
-							objectFit="contain"
-						/>
-					</div>
-					<p>Scroll to Learn More</p>
-				</div>
-				<div className="HeroSection__right">
-					<div className="HeroSection__title">
-						<h1 className="HeroSection__titleText">WE ARE THE</h1>
-						<h1 className="HeroSection__titleText">HOME OF</h1>
-						<h1 className="HeroSection__titleText HeroSection__titleText--green">CHAMPIONS</h1>
-					</div>
-					<div className="HeroSection__description">
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-						</p>
-					</div>
-				</div>
-			</section>
-			<section className="RenegadeSection">
-				<Renegade />
-			</section>
-    </div>
+      <div className="LandingPage">
+        <Header />
+        <section className="HeroSection">
+          <div
+            className="HeroSection__left"
+            style={{
+              backgroundImage: `url("/Img/HeroImgLeft.svg")`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundRepeat: "no-repeat",
+            }}
+          />
+          <img className="HeroSection__right" src="/Img/HeroImgRight.svg" />
+          <div className="HeroSection__top">
+            <h1 className="HeroSection__top--heading">
+              WE ARE THE
+              <br />
+              HOME OF
+              <br />
+              <span>CHAMPIONS</span>
+            </h1>
+            <p className="HeroSection__top--text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud
+            </p>
+            <div className="HeroSection__top--actionContainer">
+              <div className="HeroSection__top--action">
+                <img src="/Img/DownArrow.svg" />
+                <p>Scroll to Learn More</p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <SportScroll />
+        {/* <section className="RenegadeSection">
+          <Renegade />
+        </section> */}
+      </div>
+    </>
   );
 }
