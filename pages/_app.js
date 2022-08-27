16be10e5699b60dd23c492c4d01d1cd7bc2a59ef
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-27 17:54:25 +0530

    feat: Added Context wrapper to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,26 @@
 import AuthModal from "../Components/AuthModal/AuthModal";
 import "../styles/root/globals.scss";
+import { AuthProvider, getUser } from "../context/authContext";
+import App from "next/app";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, user }) {
   return (
-    <>
+    <AuthProvider ssrUser={user}>
       <AuthModal />
       <Component {...pageProps} />
-    </>
+    </AuthProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  if (appContext.router.isSsr === undefined) {
+    const appProps = await App.getInitialProps(appContext);
+    const user = await getUser(appContext.ctx);
+    return { ...appProps, user: user };
+  } else {
+    const appProps = await App.getInitialProps(appContext);
+    return { ...appProps };
+  }
+};
 
 export default MyApp;
