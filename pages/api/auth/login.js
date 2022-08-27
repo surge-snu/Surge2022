16859145d95e00abb21d85041e6f5374c618e69a
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-27 18:16:59 +0530

    feat: working login endpoint

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,1 +1,29 @@
-export default function Login(req, res) {}
+import { withIronSessionApiRoute } from "iron-session/next";
+import { ironOptions } from "../../../lib/ironOptions";
+import { compareSync } from "bcrypt";
+import db from "../../../lib/prisma";
+
+export default withIronSessionApiRoute(loginRoute, ironOptions);
+
+async function loginRoute(req, res) {
+  const { email, password } = await req.body;
+  const user = {
+    password: "$2b$09$LN3F0oodRRZNTwzLAIWvUulsUpNJRr/j2F/JlNdji7iEVyzP3bKQG",
+    email: email,
+  };
+  // const user = await db.user.findUnique({
+  //   where: {
+  //     email,
+  //   },
+  // });
+  const isMatch = await compareSync(password, user.password);
+  delete user.password;
+  if (isMatch) {
+    req.session.user = user;
+    await req.session.save();
+    res.json(user);
+  } else {
+    res.statusCode = 401;
+    res.send({ message: "Invalid email or password" });
+  }
+}
