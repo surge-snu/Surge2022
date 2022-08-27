1ca6178fc56b3b8640d9980a9caa7b2abe50f686
ABSanthosh <a.b.santhosh02@gmail.com>
2022-08-27 19:40:32 +0530

    feat: Register user endpoint

--- pages/api/auth/register.js
+++ pages/api/auth/register.js
@@ -1,8 +1,20 @@
-import { withIronSessionApiRoute } from "iron-session/next/dist";
+import { withIronSessionApiRoute } from "iron-session/next";
 import { ironOptions } from "../../../lib/ironOptions";
+import db from "../../../lib/prisma";
+import { hashSync } from "bcrypt";
 
 export default withIronSessionApiRoute(Register, ironOptions);
 
 async function Register(req, res) {
-  
+  const body = await req.body;
+  const response = await db.user.create({
+    data: {
+      name: body.friendlyName,
+      email: body.email,
+      college: "",
+      password: hashSync(body.password, 10),
+    },
+  });
+
+  res.send({ status: 200, message: JSON.stringify(response) });
 }
