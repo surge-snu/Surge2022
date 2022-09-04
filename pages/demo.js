b542441bd56724b6a5f141815de0126972c993f8
ABSanthosh <a.b.santhosh02@gmail.com>
2022-09-04 19:03:35 +0530

    demo: Testing invitations

--- pages/demo.js
+++ pages/demo.js
@@ -1,9 +1,9 @@
-import { useRouter } from "next/router";
 import React from "react";
-import useAuth from "../hooks/useAuth";
+import { sendInvitationOps } from "../operations/invitation.fetch";
+import { fetchAllUsers } from "../services/user.server";
 import "../styles/routes/Home.scss";
 
-export function getServerSideProps(context) {
+export async function getServerSideProps(context) {
   if (context.req.session.user === undefined) {
     return {
       props: {
@@ -11,17 +11,100 @@ export function getServerSideProps(context) {
       },
     };
   }
+
+  const allUsers = await fetchAllUsers();
   return {
-    props: { user: context.req.session.user },
+    props: { user: context.req.session.user, allUsers },
   };
 }
-export default function Demo({ user }) {
-  const { logout } = useAuth();
+export default function Demo({ user, allUsers }) {
+  const [player, setPlayer] = React.useState("");
+  const [possibleResults, setPossibleResults] = React.useState([]);
+  const [invitedPlayers, setInvitedPlayers] = React.useState([]);
+  const eventId = "ef0bec37-83e7-43c5-b92a-6d16a8bf2120";
+
+  React.useEffect(() => {
+    if (player !== "") {
+      const results = allUsers.filter((user) => {
+        return user.name.toLowerCase().includes(player.toLowerCase());
+      });
+      setPossibleResults(results);
+    } else {
+      setPossibleResults([]);
+    }
+  }, [player]);
 
   return (
     <main>
-      {user !== null ? <p>Hello {user.email}</p> : <a href="#login">Login</a>}
-      {user !== null && <button onClick={logout}>Logout</button>}
+      <h1>
+        {user !== null ? (
+          <p>
+            Hello {user.email} - {user.id}
+          </p>
+        ) : (
+          <a href="#login">Login</a>
+        )}
+      </h1>
+
+      <div className="InvitePlayer">
+        {invitedPlayers.map((player, index) => {
+          return (
+            <div key={index}>
+              <p>{player.name}</p>
+              <button
+                onClick={() => {
+                  const newInvitedPlayers = [...invitedPlayers];
+                  newInvitedPlayers.splice(index, 1);
+                  setInvitedPlayers(newInvitedPlayers);
+                }}
+              >
+                Remove
+              </button>
+            </div>
+          );
+        })}
+        {invitedPlayers.length > 0 && (
+          <button
+            onClick={async () => {
+              const data = invitedPlayers.map((player) => {
+                return {
+                  eventId,
+                  toId: player.id,
+                  fromId: user.id,
+                };
+              });
+
+              await sendInvitationOps(data).then((res) => console.log(res));
+            }}
+          >
+            Send Invitations
+          </button>
+        )}
+        <br />
+        <input
+          style={{ color: "black" }}
+          onChange={(e) => setPlayer(e.target.value)}
+        />
+        <ul>
+          {possibleResults
+            .filter((result) => {
+              return !invitedPlayers.includes(result);
+            })
+            .map((result, index) => {
+              return (
+                <li
+                  key={index}
+                  onClick={() => {
+                    setInvitedPlayers([...invitedPlayers, result]);
+                    setPlayer("");
+                  }}
+                >
+                  {result.name} - {result.email}
+                </li>
+              );
+            })}
+        </ul>
+      </div>
     </main>
   );
 }
--- pages/my/home.js
+++ pages/my/home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import MySidebar from "../../Components/MySidebar/MySidebar";
 import useAuth from "../../hooks/useAuth";
+import {
+  fetchInvitationsOps,
+  updateInvitationOps,
+} from "../../operations/invitation.fetch";
 import "../../styles/routes/My/My.scss";
 
 export function getServerSideProps(context) {
@@ -17,18 +21,80 @@ export function getServerSideProps(context) {
   };
 }
 export default function MyHome({ user }) {
-  const { logout, user: cUser } = useAuth();
+  const { logout } = useAuth();
+  const [sentInvitations, setSentInvitations] = React.useState([]);
+  const [receivedInvitations, setReceivedInvitations] = React.useState([]);
 
   return (
     <main>
       {user !== null ? <p>Hello {user.email}</p> : <a href="#login">Login</a>}
       {user !== null && <button onClick={logout}>Logout</button>}
+      <button
+        onClick={async () => {
+          await fetchInvitationsOps(user.email).then((res) => {
+            setReceivedInvitations(res.message.receivedInvitations);
+            setSentInvitations(res.message.sentInvitations);
+          });
+        }}
+      >
+        Refresh Invitations
+      </button>
+      <h3>Sent Invitations</h3>
+      {sentInvitations.map((invitation) => {
+        return (
+          <label key={invitation.id}>
+            {invitation.fromId} - {invitation.status}&nbsp;
+            <button
+              onClick={async () => {
+                await updateInvitationOps({
+                  id: invitation.id,
+                  status: "REVOKED",
+                });
+              }}
+            >
+              Revoke Invitation
+            </button>
+            &nbsp;
+          </label>
+        );
+      })}
+      <br />
+      <br />
+      <h3>Received Invitations</h3>
+      {receivedInvitations.map((invitation) => {
+        return (
+          <label key={invitation.id}>
+            {invitation.fromId}&nbsp;
+            <button
+              onClick={async () => {
+                await updateInvitationOps({
+                  id: invitation.id,
+                  status: "ACCEPTED",
+                });
+              }}
+            >
+              Accept
+            </button>
+            &nbsp;
+            <button
+              onClick={async () => {
+                await updateInvitationOps({
+                  id: invitation.id,
+                  status: "REJECTED",
+                });
+              }}
+            >
+              Decline
+            </button>
+            &nbsp;
+          </label>
+        );
+      })}
     </main>
   );
 }
 
 MyHome.getLayout = function getLayout(page) {
-
   return (
     <div className="MyLayout">
       <MySidebar />
