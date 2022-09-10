4a50e3d21fb292a099bd8f76859519506a44e6b4
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2022-09-10 18:19:21 +0530

    feature: Team invite
    Almost done

--- pages/event/[eventId].js
+++ pages/event/[eventId].js
@@ -48,6 +48,7 @@ export default function EventPage({ eventDetails, allUsers}) {
 	const [hash, setHash] = React.useState("");
 	
 	React.useEffect(() => {
+		setHash(window.location.hash);
     window.addEventListener("hashchange", () => {
 			setHash(window.location.hash);
     });
@@ -64,24 +65,27 @@ export default function EventPage({ eventDetails, allUsers}) {
 					
 				</>;
 			case '#team':
-				return <div className="EventPage__content--team">
+				return <>
 					<div className="EventPage__contentHeading">
 						<h3>Team</h3>
 						<div className="EventPage__contentHeading--divider"></div>
 					</div>
-					<InvitationWidget 
-						allUsers={allUsers}
-						minPlayers={eventDetails.minPlayers}
-						maxPlayers={eventDetails.maxPlayers}
-						eventId={eventDetails.eventId}
-					/>
-				</div>;
+					<div className="EventPage__content--team">
+						<InvitationWidget
+							allUsers={allUsers}
+							minPlayers={eventDetails.minPlayers}
+							maxPlayers={eventDetails.maxPlayers}
+							eventId={eventDetails.eventId}
+						/>
+					</div>
+				</>;
 			default:
 				return <div className="EventPage__content--overview">
 					<h3>Rules and Guidelines</h3>
 					<p>
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu. Curabitur pellentesque nibh nibh, at maximus ante fermentum sit amet. Pellentesque commodo lacus at sodales sodales. Quisque sagittis orci ut diam condimentum, vel euismod erat placerat. In iaculis arcu eros, eget tempus orci facilisis id.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum auctor ornare leo, non suscipit magna interdum eu.
 					</p>
+					<pre>{JSON.stringify(eventDetails, null, 2)}</pre>
 				</div>;
 		};
 	}
@@ -143,7 +147,6 @@ export default function EventPage({ eventDetails, allUsers}) {
 			<div className="EventPage__content">
 				{switchContent()}
 			</div>
-			<pre>{JSON.stringify(eventDetails, null, 2)}</pre>
 			<Footer />
 		</div>
 	</>;
