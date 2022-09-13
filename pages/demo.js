0bc53a93136c1d88e0bfeeadd437729075741502
yathansh13 <yathansh1312@gmail.com>
2022-09-13 14:45:00 +0530

    feat: Added event gist component

--- pages/demo.js
+++ pages/demo.js
@@ -3,12 +3,13 @@ import React from "react";
 import "../styles/routes/Home.scss";
 import EventCard from "../Components/EventCard/EventCard";
 import SubDetails from "../Components/SubDetails/SubDetails";
+import EventGist from "../Components/EventGist/EventGist";
 export default function Demo() {
   return (
     <>
       <main>Ok</main>
 
-      <SubDetails title="Registration cost" description="Rs 250" />
+      <EventGist />
     </>
   );
 }
