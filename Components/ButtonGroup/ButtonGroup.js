8dfebac67bf65cdf75ae16d226e777f7caedf8ca
ABSanthosh <a.b.santhosh02@gmail.com>
2022-09-26 23:18:43 +0530

    feat: new button group for events filter

---
+++ Components/ButtonGroup/ButtonGroup.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./ButtonGroup.scss";
+
+function ButtonGroup({ onFilterChange = () => {} }) {
+  const [activeFilter, setActiveFilter] = React.useState("all");
+
+  React.useEffect(() => {
+    onFilterChange(activeFilter);
+  }, [activeFilter]);
+
+  return (
+    <div className="ButtonGroupWrapper">
+      <button
+        className={`ButtonGroupWrapper__button ${
+          activeFilter === "all" ? "active" : ""
+        }`}
+        onClick={() => setActiveFilter("all")}
+      >
+        All
+      </button>
+      <button
+        className={`ButtonGroupWrapper__button ${
+          activeFilter === "male" ? "active" : ""
+        }`}
+        onClick={() => setActiveFilter("male")}
+      >
+        Male
+      </button>
+      <button
+        className={`ButtonGroupWrapper__button ${
+          activeFilter === "female" ? "active" : ""
+        }`}
+        onClick={() => setActiveFilter("female")}
+      >
+        Female
+      </button>
+      <button
+        className={`ButtonGroupWrapper__button ${
+          activeFilter === "mixed" ? "active" : ""
+        }`}
+        onClick={() => setActiveFilter("mixed")}
+      >
+        Mixed
+      </button>
+    </div>
+  );
+}
+
+export default ButtonGroup;
