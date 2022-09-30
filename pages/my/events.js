f11b5ce36f17f0ccb6f55f45cddf15f4ba30c957
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-01 01:34:58 +0530

    feat: display registered events in profile

--- pages/my/events.js
+++ pages/my/events.js
@@ -1,9 +1,12 @@
 import React from "react";
 import MySidebar from "../../Components/MySidebar/MySidebar";
-import useAuth from "../../hooks/useAuth";
+import DashHeader from "../../Components/Table/DashHeader/DashHeader";
+import DashRow from "../../Components/Table/DashRow/DashRow";
+import DashTable from "../../Components/Table/DashTable/DashTable";
+import { fetchUserData } from "../../services/user.server";
 import "../../styles/routes/My/My.scss";
 
-export function getServerSideProps(context) {
+export async function getServerSideProps(context) {
   if (context.req.session.user === undefined) {
     return {
       redirect: {
@@ -12,12 +15,55 @@ export function getServerSideProps(context) {
       },
     };
   }
+
+  const userData = await fetchUserData(context.req.session.user.email);
+
   return {
-    props: { user: context.req.session.user },
+    props: { user: userData },
   };
 }
-export default function MyEvents() {
-  return <main>Your registered events</main>;
+export default function MyEvents({ user }) {
+  // console.log(user.Team);
+
+  const [eventsDropdownIndex, setEventsDropdownIndex] = React.useState(null);
+  return (
+    <main className="MyEvents">
+      <DashTable title="Payment History">
+        <DashHeader
+          headerTitles={["Team ID", "Payment Status", "Player Count"]}
+        />
+        {user.Team.map((team, index) => (
+          <DashRow
+            key={team.teamId}
+            setDropdownIndex={setEventsDropdownIndex}
+            dropdownIndex={eventsDropdownIndex}
+            index={index}
+            contentCols={[
+              <span>{team.eventId}</span>,
+              <span>
+                {team.paymentStatus === "PAID" ? (
+                  <img src="/Img/Green Tick.svg" height={14} />
+                ) : (
+                  <img src="/Img/Red Exclamation.svg" height={14} />
+                )}
+                <p>{team.paymentStatus === "PAID" ? "Paid" : "Not Paid"}</p>
+              </span>,
+              <span>{team.TeamMembers.length}</span>,
+            ]}
+          >
+            {team.TeamMembers.map((member) => (
+              <div className="MyHome__ListTileItems">
+                <p>{member.name}</p>{" - "}
+                <p>{member.playerType}</p>
+              </div>
+            ))}
+
+            <button className="MyHome__greenButton">Download Invoice</button>
+          </DashRow>
+        ))}
+      </DashTable>
+    </main>
+  );
 }
 
 MyEvents.getLayout = function getLayout(page) {
