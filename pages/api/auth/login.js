8f02c0eefc8b05619aa1d67f4f277f121ff846aa
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-02 22:18:49 +0530

    fix: Added full user data to context store

--- pages/api/auth/login.js
+++ pages/api/auth/login.js
@@ -1,7 +1,7 @@
 import { withIronSessionApiRoute } from "iron-session/next";
 import { ironOptions } from "../../../lib/ironOptions";
 import { compareSync } from "bcrypt";
-import { fetchUser } from "../../../services/user.server";
+import { fetchUser, fetchUserData } from "../../../services/user.server";
 
 export default withIronSessionApiRoute(loginRoute, ironOptions);
 
@@ -16,14 +16,15 @@ async function loginRoute(req, res) {
     return res.status(400).json({
       status: 400,
       message: "User not found, try signing up...",
-    }); 
+    });
   }
   const isMatch = await compareSync(password, user.password);
   delete user.password;
   if (isMatch) {
     req.session.user = user;
     await req.session.save();
-    res.json({ status: 200, user });
+    const fullUserData = await fetchUserData(user.email);
+    res.json({ status: 200, user: fullUserData });
   } else {
     res.statusCode = 401;
     res.send({ status: 401, message: "Invalid email or password" });
