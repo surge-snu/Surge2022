0c4fe320f177925aee5b4ab175bce13892c05a2b
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-08 19:44:44 +0530

    chore: small formatting

--- utils/Cashify.js
+++ utils/Cashify.js
@@ -1,5 +1,5 @@
 export const Cashify = (num) => {
-  return `₹${new Intl.NumberFormat("en-IN", {
+  return `₹ ${new Intl.NumberFormat("en-IN", {
     maximumSignificantDigits: 3,
   }).format(num)}`;
 };
