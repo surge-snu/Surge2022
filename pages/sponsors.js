5bf151310bae49d05967dcb95767ba7b3cbf4892
ABSanthosh <a.b.santhosh02@gmail.com>
2022-10-09 01:06:49 +0530

    feat: sponsors page done

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -11,7 +11,53 @@ export async function getServerSideProps(context) {
 }
 
 export default function Sponsors() {
-  return <div className="SponsorsPage__container">Sponsors</div>;
+  return (
+    <div className="SponsorsPage__container">
+      <section className="SponsorsPage__section">
+        <h2 className="SponsorsPage__section--header">Past Sponsors</h2>
+        <div className="SponsorsPage__section--sponsContainer">
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/Bingo.png" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/CanvasLaughClub.png" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/Cisco.png" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/Jio.svg" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img
+              className="widthFill"
+              src="/Img/Spons/Cognizant.svg"
+              alt="sponsor"
+            />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/SouledStore.png" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/Gail.png" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/Saavn.png" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img src="/Img/Spons/CocaCola.jpg" alt="sponsor" />
+          </div>
+          <div className="SponsorsPage__section--spons">
+            <img
+              className="widthFill"
+              src="/Img/Spons/Systra.svg"
+              alt="sponsor"
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 Sponsors.getLayout = function getLayout(page) {
