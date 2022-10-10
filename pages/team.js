dc26c8b06ff98f64958cb361e8a3d7ad435c3412
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2022-10-10 20:23:26 +0530

    update: Responsiveness & refactoring

--- pages/team.js
+++ pages/team.js
@@ -1,7 +1,7 @@
+import React from "react";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/Header/Header";
 import "../styles/routes/Team.scss";
-import TeamGrid from "../Components/TeamGrid/TeamGrid";
 
 export async function getServerSideProps(context) {
   return {
@@ -12,45 +12,82 @@ export async function getServerSideProps(context) {
 }
 
 export default function Team() {
+	
+	const teams = ["all", "content", "marketing", "design", "pr", "webdev"];
+	const [team, setTeam] = React.useState(teams[0]);
+	
+	let members = [...Array(20).keys()]
+		.map((v) => (
+			{
+				"name": "Dark Lord " + v,
+				"position": "Web Development Lead",
+				"team": teams[(v % 5) + 1],
+			}
+		));
+	
+	
   return (
-    <div className="TeamPage">
-      <Header />
+    <>
       <div className="TeamPage__content">
-        <p className="TeamPage__content__heading">
+        <p className="TeamPage__heading">
           MEET THE{" "}
-          <span className="TeamPage__content__heading__change">
-            TEAM BEHIND
+          <span className="TeamPage__heading--green">
+            TEAM<br /> BEHIND
           </span>{" "}
           THE MAGIC
         </p>
-        <p className="TeamPage__content__desc">
-          {" "}
+        <p className="TeamPage__desc">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
-        <div className="TeamPage__content__scrollbar">
-          <a className="TeamPage__content__scrollbar__element">All</a>
-          <a className="TeamPage__content__scrollbar__element"> Content</a>
-          <a className="TeamPage__content__scrollbar__element">Marketing</a>
-          <a className="TeamPage__content__scrollbar__element">Design</a>
-
-          <a className="TeamPage__content__scrollbar__element">
-            PR & Sponsorship
-          </a>
-          <a className="TeamPage__content__scrollbar__element">
-            Web Development
-          </a>
+        <div className="TeamPage__categories">
+					<button
+						className={`TeamPage__category ${team === teams[0] ? "TeamPage__category--active" : ""}`}
+						onClick={() => setTeam(teams[0])}
+					>All</button>
+					<button
+						className={`TeamPage__category ${team === teams[1] ? "TeamPage__category--active" : ""}`}
+						onClick={() => setTeam(teams[1])}
+					>Content</button>
+					<button
+						className={`TeamPage__category ${team === teams[2] ? "TeamPage__category--active" : ""}`}
+						onClick={() => setTeam(teams[2])}
+					>Marketing</button>
+					<button
+						className={`TeamPage__category ${team === teams[3] ? "TeamPage__category--active" : ""}`}
+						onClick={() => setTeam(teams[3])}
+					>Design</button>
+					<button
+						className={`TeamPage__category ${team === teams[4] ? "TeamPage__category--active" : ""}`}
+						onClick={() => setTeam(teams[4])}
+					>PR & Sponsorship</button>
+					<button
+						className={`TeamPage__category ${team === teams[5] ? "TeamPage__category--active" : ""}`}
+						onClick={() => setTeam(teams[5])}
+					>Web Development</button>
         </div>
       </div>
-      <TeamGrid />
-    </div>
+      <div className="TeamPage__grid">
+				{members
+					.filter((member) => team === teams[0] || member.team === team)
+					.map((member) => (
+						<div className="TeamPage__card">
+							<img src="/Img/team.svg" className="TeamPage__cardImg" />
+							<p className="TeamPage__cardName"> {member["name"]}</p>
+							<p className="TeamPage__cardDesc">{member["position"]}</p>
+						</div>
+					))
+				}
+			</div>
+    </>
   );
 }
 
-// Team.getLayout = function getLayout(page) {
-//   return (
-//     <div className="TeamPage">
-//       <Header currentPath={page.props.currentPath} />
-//       {page}
-//     </div>
-//   );
-// };
+Team.getLayout = function getLayout(page) {
+  return (
+    <div className="TeamPage">
+      <Header currentPath={page.props.currentPath} />
+			{page}
+			<Footer />
+    </div>
+  );
+};
