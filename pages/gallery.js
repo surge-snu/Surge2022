bfee240d90a987e25cb3dca393f4f8fa29e9b795
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2022-10-19 17:42:06 +0530

    feature: gallery page

--- pages/gallery.js
+++ pages/gallery.js
@@ -3,17 +3,45 @@ import Header from "../Components/Header/Header";
 import "../styles/routes/Gallery.scss";
 
 export async function getServerSideProps(context) {
+	let images = require.context("../public/Gallery");
   return {
     props: {
-      currentPath: context.req.url,
+			currentPath: context.req.url,
+			images: images.keys().map((item, index) => item.replace("./", "")),
     },
-  };
+	};
 }
 
-export default function Gallery() {
+export default function Gallery(props) {
+	console.log(props.images);
   return (
     <div className="GalleryPage__container">
-      <h3>Pictures coming soon!</h3>
+      <div className="GalleryPage__container">
+				<p className="GalleryPage__heading">
+					MEET THE{" "}
+					<span className="GalleryPage__heading--green">
+						TEAM
+						<br /> BEHIND
+					</span>{" "}
+					THE MAGIC
+				</p>
+				<div className="GalleryPage__grid">
+					{props.images.map((image, index) => (
+						<div className="GalleryPage__imageContainer">
+							<img
+								key={image}
+								className="GalleryPage__image"
+								src={`/Gallery/${image}`}
+								alt="Gallery"
+								loading="lazy"
+							/>
+							<p>
+								{image.split(".")[0]}
+							</p>
+						</div>
+					))}
+				</div>
+			</div>
     </div>
   );
 }
