00d97a12256e5d474d69f4e1949df7c32401f638
Rishi Balamurugan <45452035+Crazzygamerr@users.noreply.github.com>
2022-11-09 14:09:50 +0530

    update: Sponsor data

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -3,69 +3,62 @@ import Header from "../Components/Header/Header";
 import "../styles/routes/Sponsors.scss";
 
 export async function getServerSideProps(context) {
-  return {
-    props: {
-      currentPath: context.req.url,
-    },
-  };
+	let spons = require.context("../public/Img/Sponsors");
+	let pastSpons = require.context("../public/Img/Spons");
+	return {
+		props: {
+			currentPath: context.req.url,
+			spons: spons.keys().map((item, index) => item.replace("./", "")),
+			pastSpons: pastSpons.keys().map((item, index) => item.replace("./", "")),
+		},
+	};
 }
 
-export default function Sponsors() {
-  return (
-    <div className="SponsorsPage__container">
-      <section className="SponsorsPage__section">
-        <h2 className="SponsorsPage__section--header">Past Sponsors</h2>
-        <div className="SponsorsPage__section--sponsContainer">
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/Bingo.png" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/CanvasLaughClub.png" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/Cisco.png" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/Jio.svg" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img
-              className="widthFill"
-              src="/Img/Spons/Cognizant.svg"
-              alt="sponsor"
-            />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/SouledStore.png" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/Gail.png" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/Saavn.png" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img src="/Img/Spons/CocaCola.jpg" alt="sponsor" />
-          </div>
-          <div className="SponsorsPage__section--spons">
-            <img
-              className="widthFill"
-              src="/Img/Spons/Systra.svg"
-              alt="sponsor"
-            />
-          </div>
-        </div>
-      </section>
-    </div>
-  );
+export default function Sponsors(props) {
+	return (
+		<div className="SponsorsPage__container">
+			<section className="SponsorsPage__section">
+				<h2 className="SponsorsPage__section--header">Sponsors</h2>
+				<div className="SponsorsPage__section--sponsContainer">
+					{props.spons.map((item, index) => {
+						return (
+							<div className="SponsorsPage__section--spons" key={index}>
+								<img
+									src={`/Img/sponsors/${item}`}
+									alt="sponsor"
+									objectFit="contain"
+									className="widthFill"
+								/>
+							</div>
+						);
+					})}
+				</div>
+				<h2 className="SponsorsPage__section--header">Past Sponsors</h2>
+				<div className="SponsorsPage__section--sponsContainer">
+					{props.pastSpons.map((item, index) => {
+						return (
+							<div className="SponsorsPage__section--spons" key={index}>
+								<img
+									src={`/Img/Spons/${item}`}
+									alt="sponsor"
+									objectFit="contain"
+									className="widthFill"
+								/>
+							</div>
+						);
+					})}
+				</div>
+			</section>
+		</div>
+	);
 }
 
 Sponsors.getLayout = function getLayout(page) {
-  return (
-    <div className="SponsorsPage">
-      <Header currentPath="sponsors" />
-      {page}
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="SponsorsPage">
+			<Header currentPath="sponsors" />
+			{page}
+			<Footer />
+		</div>
+	);
 };
